ed0d9e9e02d1f0d766902585b43c1952e057ef67
Your Name <lirannh@gmail.com>
2019-04-28 13:32:03 +0300

    change project

---
+++ TechMaven/src/main/java/TechMaven/TechMaven/boundries/SelectMove.java
@@ -0,0 +1,15 @@
+package TechMaven.TechMaven.boundries;
+
+import java.util.Scanner;
+
+public class SelectMove
+{
+    public int GetMove() {
+        // ----------- << method.body@AAAAAAFqEXQmdBefqFE= >>
+        // ----------- >>
+            Scanner s = new Scanner(System.in);
+            int postions = s.nextInt();
+
+            return postions;
+        }
+}
